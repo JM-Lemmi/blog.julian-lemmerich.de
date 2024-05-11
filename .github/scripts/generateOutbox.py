02287213a83eae2e7705183a8b3f4ee6ac5cbdcd
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2024-05-11 22:10:50 +0200

    [ci] generate Activitypub Outbox

---
+++ .github/scripts/generateOutbox.py
@@ -0,0 +1,46 @@
+import os
+import json
+
+files = os.listdir(os.getcwd())
+files = [file for file in files if file.endswith(".json")]
+
+notes = []
+
+for file in files:
+    with open(file, 'r') as f:
+        metadata = json.load(f)
+        url = "https://blog.julian-lemmerich.de/" + file.replace('.json', '.html')
+        
+        outboxnote = {
+            "@context": "https://www.w3.org/ns/activitystreams",
+            "id": "https://blog.julian-lemmerich.de/activitypub/notes/" + file,
+            "type": "Note",
+            "content": metadata['description'] + "\n\n" + url,
+            "url": url,
+            "attributedTo": "https://blog.julian-lemmerich.de/activitypub/actor.json",
+            "to": [
+                "https://www.w3.org/ns/activitystreams#Public"
+            ],
+            "cc": [],
+            "published": metadata['date'] + "T00:00:00Z",
+            "tag": []
+        }
+
+        with open(os.getcwd() + "/dist/activitypub/notes/" + file, "w+") as f:
+            json.dump(outboxnote, f)
+            print("Done, saved outbox note to " + os.getcwd() + "/dist/activitypub/notes/" + file)
+        
+        notes.append(outboxnote)
+
+outbox = {
+    "@context": "https://www.w3.org/ns/activitystreams",
+    "id": "https://blog.julian-lemmerich.de/activitypub/outbox.json",
+    "type": "OrderedCollection",
+    "summary": "Personal blog by Julian Lemmerich.",
+    "totalItems": len(notes),
+    "orderedItems": notes
+}
+
+with open(os.getcwd() + "/dist/activitypub/outbox.json", "w+") as file:
+    json.dump(outbox, file)
+    print("Done, saved outbox to " + os.getcwd() + "/dist/activitypub/outbox.json")
